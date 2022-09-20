52008c72b97a06f2260b8747a3e0866c5e13fe5c
ArashkKH <106018197+ArashkKH@users.noreply.github.com>
2022-09-20 12:30:33 +0430

    test

--- react-exercise/src/Pages/main2/main2.jsx
+++ react-exercise/src/Pages/main2/main2.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import './main2.css'
 import DivImg from './asset/division image.jpg'
+// import { useRef } from "react";
 //menuButtons imgs
 import ImgHot from './asset/whats hot.webp'
 import ImgBurgers from './asset/burgers.webp'
@@ -11,9 +12,17 @@ import Imgdrinks from './asset/drinks.webp'
 
 
 export default function App(){
+    const test = useRef()
+    // console.log(test.current)
 
-
-
+    const buttons = document.getElementsByClassName('choice')
+    console.log(buttons)
+    // buttons.forEach((element,index) => {
+    //     // element.setAttribute('data-num',index)
+    //     // console.log(element)
+    // });
+    // test.current.setAttribute('class','ddd')
+    
     return(
         <>
             <div className="row" id="lv3">
@@ -22,27 +31,27 @@ export default function App(){
                     <div className="menuHeader">
                             <div className="Menutitle">Pesto's Menu</div>
                             <div className="menuButtons">
-                                <div className="choice C-hot">
+                                <div  className="choice C-hot choosen" ref={test} >
                                     <figure><img src={ImgHot} alt="" /></figure>
                                     <span className="choiceName">What's Hot</span>
                                 </div>
-                                <div className="choice C-burgers">
+                                <div  className="choice C-burgers" >
                                     <figure><img src={ImgBurgers} alt="" /></figure>
                                     <span className="choiceName">Burgers</span>
                                 </div>
-                                <div className="choice C-chickens">
+                                <div  className="choice C-chickens">
                                     <figure><img src={Imgnuggs} alt="" /></figure>
                                     <span className="choiceName">Chickens and Salads</span>
                                 </div>
-                                <div className="choice C-tacos">
+                                <div  className="choice C-tacos">
                                     <figure><img src={Imgtacos} alt="" /></figure>
                                     <span className="choiceName">Tacos, Fries and Sides</span>
                                 </div>
-                                <div className="choice C-breakfasts">
+                                <div  className="choice C-breakfasts">
                                     <figure><img src={ImgBreakfasts} alt="" /></figure>
                                     <span className="choiceName">Breakfast</span>
                                 </div>
-                                <div className="choosen choice C-drinks">
+                                <div  className="choice C-drinks">
                                     <figure><img src={Imgdrinks} alt="" /></figure>
                                     <span className="choiceName">Desserts and Drinks</span>
                                 </div>
